437c00f5e585fa0963bb72c3a00e74fe867bd727
John Lu <johnlu2006@gmail.com>
2023-08-16 14:57:45 -0500

    Console.ReadLine and string interpolation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Thrown for a Loop!");
-Console.WriteLine("Please choose an option: ");
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+Console.WriteLine("Please enter a product name: ");
+
+string response = Console.ReadLine(); 
+/* 这一行, 很关键, ReadLine是阅读用户的input(在hit enter之前) */
+Console.WriteLine($"You chose: {response}");
+
+Console.WriteLine(@$"You chose: {response}. 
+Thank you for your input!");
+/* 这一行, string interpolation */
