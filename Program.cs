25088119e0e3c5e7c27a641a2d4da914e2067360
John Lu <johnlu2006@gmail.com>
2023-08-17 11:59:31 -0500

    chapter 7 DateTime

--- Program.cs
+++ Program.cs
@@ -10,16 +10,20 @@ Console.WriteLine(greeting);
 List<Product> products = new List<Product>()
 {
     new Product()
-    { 
-        Name = "Football", 
-        Price = 15, 
-        Sold = false
+    {
+        Name = "Football",
+        Price = 15,
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     },
-    new Product() 
-    { 
-        Name = "Hockey Stick", 
-        Price = 12, 
-        Sold = false
+    new Product()
+    {
+        Name = "Hockey Stick",
+        Price = 12,
+        Sold = false,
+        StockDate = new DateTime(2022, 12, 21),
+        ManufactureYear = 2011
     }
 };
 
@@ -32,7 +36,7 @@ for (int i = 0; i < products.Count; i++)
 
 Console.WriteLine("Please enter a product number: ");
 
-int response = int.Parse( Console.ReadLine().Trim());
+int response = int.Parse(Console.ReadLine().Trim());
 /* 这一行, 很关键, ReadLine是阅读用户的input(在hit enter之前) */
 // int.Parse是把string转为int
 
@@ -40,11 +44,18 @@ while (response > products.Count || response < 1)
 {
     Console.WriteLine("Choose a number between 1 and 5!");
     response = int.Parse(Console.ReadLine().Trim());
-        // 可以reassign
+    // 可以reassign
 }
 
 Product chosenProduct = products[response - 1];
-Console.WriteLine($"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars and is {(chosenProduct.Sold ? "" : "not ")}sold.");
+
+DateTime now = DateTime.Now;
+
+TimeSpan timeInStock = now - chosenProduct.StockDate;
+Console.WriteLine(@$"You chose: 
+{chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
+It is {now.Year - chosenProduct.ManufactureYear} years old. 
+It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
 
 
 
