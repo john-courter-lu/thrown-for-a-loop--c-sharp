134a62000cbd6cd1a8374debea8d02140e4d0799
John Lu <johnlu2006@gmail.com>
2023-08-17 16:14:44 -0500

    chapter 10 methods; filter products with if.. new List.Add();

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
+﻿
 string greeting = @"Welcome to Thrown For a Loop
 Your one-stop shop for used sporting equipment";
 
@@ -23,69 +20,132 @@ List<Product> products = new List<Product>()
         Name = "Hockey Stick",
         Price = 12.00M,
         Sold = false,
-        StockDate = new DateTime(2022, 12, 21),
+        StockDate = new DateTime(2023, 7, 21),
         ManufactureYear = 2011,
         Condition = 3.5
     }
 };
 
-Console.WriteLine("Products:");
-
-for (int i = 0; i < products.Count; i++)
+string choice = null;
+while (choice != "0")
 {
-    Console.WriteLine($"{i + 1}. {products[i].Name}");
-}
-
-Product chosenProduct = null;
-
-while (chosenProduct == null)
-{
-    Console.WriteLine("Please enter a product number: ");
-    try
+    Console.WriteLine(@"Choose an option:
+                        0. Exit
+                        1. View All Products
+                        2. View Product Details
+                        3. View Latest Products");
+    choice = Console.ReadLine();
+    if (choice == "0")
     {
-        int response = int.Parse(Console.ReadLine().Trim());
-        /* 这一行, 很关键, ReadLine是阅读用户的input(在hit enter之前) */
-        // int.Parse是把string转为int
-        chosenProduct = products[response - 1];
+        Console.WriteLine("Goodbye!");
     }
-    catch (FormatException)
+    else if (choice == "1")
     {
-        Console.WriteLine("Please type only integers!");
+        ListProducts();
     }
-    catch (ArgumentOutOfRangeException)
+    else if (choice == "2")
     {
-        Console.WriteLine("Please choose an existing item only!");
+        ViewProductDetails();
     }
-    catch (Exception ex)
-    // 如果try里面有runtime error/Exceptions. 那就执行catch里面的code
+    else if (choice == "3")
     {
-        Console.WriteLine(ex);
-        //这样就不会看到unhandled exception字样了. code也不会停止运行了.
-        Console.WriteLine("Do better!");
+        ViewLatestProducts();
     }
 }
+// while可以让其中的函数重复运行直到 choice == "0",
+// 这样就可以一直选择下去.
+
+
+
+void ViewProductDetails()
+{
+
+    ListProducts();
+
+    Product chosenProduct = null;
+
+    while (chosenProduct == null)
+    {
+        Console.WriteLine("Please enter a product number: ");
+        try
+        {
+            int response = int.Parse(Console.ReadLine().Trim());
+            /* 这一行, 很关键, ReadLine是阅读用户的input(在hit enter之前) */
+            // int.Parse是把string转为int
+            chosenProduct = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please type only integers!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing item only!");
+        }
+        catch (Exception ex)
+        // 如果try里面有runtime error/Exceptions. 那就执行catch里面的code
+        {
+            Console.WriteLine(ex);
+            //这样就不会看到unhandled exception字样了. code也不会停止运行了.
+            Console.WriteLine("Do better!");
+        }
+    }
 
 
 
 
 
-DateTime now = DateTime.Now;
+    DateTime now = DateTime.Now;
 
-TimeSpan timeInStock = now - chosenProduct.StockDate;
-Console.WriteLine(@$"You chose: 
+    TimeSpan timeInStock = now - chosenProduct.StockDate;
+    Console.WriteLine(@$"You chose: 
 {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
 It is {now.Year - chosenProduct.ManufactureYear} years old. 
 It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}
 Its condition is {chosenProduct.Condition} out of 5.");
 
-/*这一行, string interpolation */
+    /*这一行, string interpolation */
 
-decimal totalValue = 0.0M;
-foreach (Product product in products)
+
+
+}
+
+void ListProducts()
 {
-    if (!product.Sold)
+    decimal totalValue = 0.0M;
+    foreach (Product product in products)
     {
-        totalValue += product.Price;
+        if (!product.Sold)
+        {
+            totalValue += product.Price;
+        }
+    }
+    Console.WriteLine($"Total inventory value: ${totalValue}");
+    Console.WriteLine("Products:");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
     }
 }
-Console.WriteLine($"Total inventory value: ${totalValue}");
+
+void ViewLatestProducts()
+{
+    // create a new empty List to store the latest products
+    List<Product> latestProducts = new List<Product>();
+    // Calculate a DateTime 90 days in the past
+    DateTime threeMonthsAgo = DateTime.Now - TimeSpan.FromDays(90);
+    //loop through the products
+    foreach (Product product in products)
+    {
+        //Add a product to latestProducts if it fits the criteria
+        if (product.StockDate > threeMonthsAgo && !product.Sold)
+        {
+            latestProducts.Add(product);
+        }
+    }
+    // print out the latest products to the console 
+    for (int i = 0; i < latestProducts.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {latestProducts[i].Name}");
+    }
+}
