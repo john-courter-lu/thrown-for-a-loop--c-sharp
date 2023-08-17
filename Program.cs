7e5b497c73f383a991fdf907eeddb3baa85f9494
John Lu <johnlu2006@gmail.com>
2023-08-17 11:16:43 -0500

    chapter 4 integer

--- Program.cs
+++ Program.cs
@@ -7,14 +7,23 @@ Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
 
-Console.WriteLine("Please enter a product name: ");
+Console.WriteLine(@"Products:
+1. Football
+2. Hockey Stick
+3. Boomerang
+4. Frisbee
+5. Golf Putter");
 
-string response = Console.ReadLine().Trim();
+Console.WriteLine("Please enter a product number: ");
+
+int response = int.Parse( Console.ReadLine().Trim());
 /* 这一行, 很关键, ReadLine是阅读用户的input(在hit enter之前) */
-while (string.IsNullOrEmpty(response))
+// int.Parse是把string转为int
+
+while (response > 5 || response < 1)
 {
-    Console.WriteLine("You didn't choose anything! Try again");
-        response = Console.ReadLine().Trim();
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
         // 可以reassign
 }
 
